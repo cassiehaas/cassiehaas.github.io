b89c3d99a26877268ad1d78adc15e5875049006d
cassiehaas <cassie.a.haas@gmail.com>
2025-01-08 11:21:05 -0600

    Button works, formatting change

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let input;
+
 function getValue() {
     const inputElement = document.getElementById("myInput");
     const inputValue = inputElement.value;
   
-    let input = ("You entered: " + inputValue);
-    document.write(input)
+    input = ("You entered: " + inputValue);
+    document.getElementById("output").innerHTML = `${input}`;
   }
