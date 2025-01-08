628ce13e193376da20977113fe6f92883a867476
cassiehaas <cassie.a.haas@gmail.com>
2025-01-07 23:38:18 -0600

    Began testing CSS and JS, broke up into multiple files

---
+++ script.js
@@ -0,0 +1,7 @@
+function getValue() {
+    const inputElement = document.getElementById("myInput");
+    const inputValue = inputElement.value;
+  
+    let input = ("You entered: " + inputValue);
+    document.write(input)
+  }
